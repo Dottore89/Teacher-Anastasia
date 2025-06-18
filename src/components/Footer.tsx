d8a38756455514c21e7848a2b4e6c1fec5ff770a
Dottore89 <115156475+Dottore89@users.noreply.github.com>
2025-06-18 14:40:13 +0200

    First Upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// 8
+export default function Footer() {
+    return (
+        <div className="bg-[#d5a021] flex justify-center gap-5 lg:gap-20 text-xs px-4 py-2 text-[#ede7d9] mt-24 lg:px-8 lg:py-4 lg:text-base">
+            <h4 className="cursor-pointer">Terms of Service</h4>
+            <h4 className="cursor-pointer">Privacy policy</h4>
+            <h4 className="cursor-pointer">Cookies</h4>
+            <h4>teacheranastasia90@gmail.com +7 685987425</h4>
+            <h4>All rights reserved © 2025 Teacher Anastasia</h4>
+        </div>
+    )
+}
